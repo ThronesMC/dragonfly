5acc7532b2a3c804b78a2c26af5575f2b595f173
Stodgy <stodgytomcat561@gmail.com>
2024-01-14 00:24:21 +0200

    fix magma block identifier

--- server/block/magma.go
+++ server/block/magma.go
@@ -35,11 +35,11 @@ func (m Magma) EntityInside(_ cube.Pos, _ *world.World, e world.Entity) {
 }
 
 func (Magma) EncodeItem() (name string, meta int16) {
-	return "minecraft:magma_block", 0
+	return "minecraft:magma", 0
 }
 
 func (Magma) EncodeBlock() (string, map[string]any) {
-	return "minecraft:magma_block", nil
+	return "minecraft:magma", nil
 }
 
 // MagmaDamageSource is used for damage caused by being on Magma.
