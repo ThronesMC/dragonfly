7540caf011cd5a3ec43b895bb79516ceca3894b1
Stodgy <80104967+Studgi@users.noreply.github.com>
2024-02-09 12:50:23 +0200

    debug

--- server/world/entity.go
+++ server/world/entity.go
@@ -1,7 +1,6 @@
 package world
 
 import (
-	"fmt"
 	"github.com/df-mc/dragonfly/server/block/cube"
 	"github.com/go-gl/mathgl/mgl64"
 	"golang.org/x/exp/maps"
@@ -146,7 +145,6 @@ func (reg EntityRegistry) Types() []EntityType {
 }
 
 func (reg EntityRegistry) AddType(name string, et EntityType) {
-	fmt.Println(reg.ent)
 	if _, ok := reg.ent[name]; ok {
 		panic("cannot register the same entity (" + name + ") twice")
 	}
